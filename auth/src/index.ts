302d7a169d8c18440e47dbef633b8b8f191eda34
Samb-PC <samb21272@gmail.com>
2021-12-25 18:33:40 +0330

    add ingress config!

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import {json} from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req: any, res: any, next: any) => {
+    res.send('first test for microservice');
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000!');
 });
